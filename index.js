8b8acb208c6ec2274b4bc1406de1eb9ebf897bc2
mailubb <mailub2004@gmail.com>
2024-08-08 18:56:59 -0500

    algo poquito

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-//Lista de palabras para randomizar
-let words = ["papaya", "mango", "cereza"];
+// trayendo cosas del html
+const wordInput = document.getElementById('wordInput');
+const wordButton = document.getElementById('scrambleButton');
+const wordDisplay = document.getElementById('resetButton');
+const wordScramble = document.getElementById('randomWord');
+
+// Lista de palabras para randomizar
+let characters = ["papaya", "mango", "cereza"];
 
 // Función para mezclar una palabra
-function scrambleWord(word) {
-    let characters = word.split('');
-    for (let i = characters.length - 1; i > 0; i--) {
+function shuffleWord(word) {
+    let array = word.split('');
+    for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [characters[i], characters[j]] = [characters[j], characters[i]];
+        [array[i], array[j]] = [array[j], array[i]];
     }
-    return characters.join('');
+    return array.join('');
 }
- console.log(scrambleWord(words[0]));
+
+// boton para randomizar palabra
+wordButton.addEventListener('click', () => {
+    // selecciona una palabra del arreglo y le pongo un indez random
+    const randomIndex = Math.floor(Math.random() * 3)
+    const selectedWord = characters[characters.length - 1 - randomIndex]
+
+    // mezclar las letras de la palabra seleccionada
+    const shuffledWord = shuffleWord(selectedWord)
+    console.log(selectedWord)
+
+    // Mostrar la palabra mezclada
+    wordScramble.textContent = `Palabra mezclada: ${shuffledWord}`
+})
+
+    
